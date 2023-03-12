2c0e3cdfd269bed4890198838edeb3ff66b703ba
egor <svitov.ev@phystech.edu>
2023-03-12 19:56:52 +0300

    upd

---
+++ 2.1.3/processing.py
@@ -0,0 +1,111 @@
+from matplotlib import pyplot as plt
+from os import listdir
+from os.path import isfile, join
+import numpy as np
+
+def k_error(x, y, k, n):
+    return np.sqrt((np.mean(y**2) - np.mean(y)**2)/(np.mean(x**2) - np.mean(x)**2) - k**2)/np.sqrt(n)
+
+n = []
+v1 = []
+v2 = []
+v3 = []
+v4 = []
+
+with open("dat.txt", "r") as file:
+    lines = file.readlines()
+    for line in lines:
+        [n_, v1_, v2_, v3_, v4_] = [float(elem) for elem in line.split()]
+        n.append(n_)
+        v1.append(v1_)
+        v2.append(v2_)
+        v3.append(v3_)
+        v4.append(v4_)
+
+n_n = np.array(n)
+v1_n = np.array(v1)
+v2_n = np.array(v2)
+v3_n = np.array(v3)
+v4_n = np.array(v4)
+
+k1, b1 = np.polyfit(n, v1_n, 1)
+k2, b2 = np.polyfit(n, v2_n, 1)
+k3, b3 = np.polyfit(n, v3_n, 1)
+k4, b4 = np.polyfit(n, v4_n, 1)
+
+k1_rand = k_error(n_n, v1_n, k1, 5)
+k2_rand = k_error(n_n, v2_n, k2, 5)
+k3_rand = k_error(n_n, v3_n, k3, 5)
+k4_rand = k_error(n_n, v4_n, k4, 5)
+
+mnk_x = [0, max(n_n)]
+mnk_y1 = [b1, mnk_x[1]*k1 + b1]
+mnk_y2 = [b2, mnk_x[1]*k2 + b2]
+mnk_y3 = [b3, mnk_x[1]*k3 + b3]
+mnk_y4 = [b4, mnk_x[1]*k4 + b4]
+
+fig, pt = plt.subplots(figsize=(12, 8), dpi=400)
+# pt.axis(xmin=0, ymin=0)
+pt.set_xlabel('$n$')
+pt.set_ylabel('$\\nu$')
+pt.errorbar(n_n, v1_n, fmt='o', yerr=0.1, label='Температура ??? С')
+pt.plot(mnk_x, mnk_y1)
+pt.errorbar(n_n, v2_n, fmt='o', yerr=0.1, label='Температура ??? С')
+pt.plot(mnk_x, mnk_y2)
+pt.errorbar(n_n, v3_n, fmt='o', yerr=0.1, label='Температура ??? С')
+pt.plot(mnk_x, mnk_y3)
+pt.errorbar(n_n, v4_n, fmt='o', yerr=0.1, label='Температура ??? С')
+pt.plot(mnk_x, mnk_y4)
+pt.minorticks_on()
+pt.grid(which='major')
+pt.grid(which='minor', linestyle=':')
+pt.legend()
+fig.savefig('all_in_one.png')
+
+fig, pt = plt.subplots(figsize=(8, 6), dpi=400)
+# pt.axis(xmin=0, ymin=0)
+pt.set_xlabel('$n$')
+pt.set_ylabel('$\\nu$')
+pt.errorbar(n_n, v1_n, fmt='o', yerr=0.1, label='Температура ??? С')
+pt.plot(mnk_x, mnk_y1)
+pt.minorticks_on()
+pt.grid(which='major')
+pt.grid(which='minor', linestyle=':')
+pt.legend()
+fig.savefig('first.png')
+
+fig, pt = plt.subplots(figsize=(8, 6), dpi=400)
+# pt.axis(xmin=0, ymin=0)
+pt.set_xlabel('$n$')
+pt.set_ylabel('$\\nu$')
+pt.errorbar(n_n, v2_n, fmt='o', yerr=0.1, label='Температура ??? С')
+pt.plot(mnk_x, mnk_y2)
+pt.minorticks_on()
+pt.grid(which='major')
+pt.grid(which='minor', linestyle=':')
+pt.legend()
+fig.savefig('second.png')
+
+fig, pt = plt.subplots(figsize=(8, 6), dpi=400)
+# pt.axis(xmin=0, ymin=0)
+pt.set_xlabel('$n$')
+pt.set_ylabel('$\\nu$')
+pt.errorbar(n_n, v3_n, fmt='o', yerr=0.1, label='Температура ??? С')
+pt.plot(mnk_x, mnk_y3)
+pt.minorticks_on()
+pt.grid(which='major')
+pt.grid(which='minor', linestyle=':')
+pt.legend()
+fig.savefig('third.png')
+
+fig, pt = plt.subplots(figsize=(8, 6), dpi=400)
+# pt.axis(xmin=0, ymin=0)
+pt.set_xlabel('$n$')
+pt.set_ylabel('$\\nu$')
+pt.errorbar(n_n, v4_n, fmt='o', yerr=0.1, label='Температура ??? С')
+pt.plot(mnk_x, mnk_y4)
+pt.minorticks_on()
+pt.grid(which='major')
+pt.grid(which='minor', linestyle=':')
+pt.legend()
+fig.savefig('fourth.png')
